fde00393b3d62a3cc2e8049dfdfcf70ecf456d18
Champion2005 <patel@apatel.xyz>
2025-02-18 17:41:06 -0500

    added file frontend and made site more responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ const Footer = () => {
     return (
         <footer className="bg-section-base text-center p-4">
             <p className="text-black/50">© 2025 - All rights reserved</p>
+            <p className="text-black/50 text-sm">Created by <a href="https://apatel.xyz" className="text-info" target="none"><u>Aditya Patel</u></a></p>
         </footer>
     )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,73 +1,164 @@
 import React from "react";
 import { FaCrown } from "react-icons/fa6";
+import { IoMenu, IoCloseOutline } from "react-icons/io5";
 
 const NavBar = ({ selected, setSelected }) => {
+  const [screenWidth, setScreenWidth] = React.useState(window.innerWidth);
+  const [showMenu, setShowMenu] = React.useState(false);
+
   const navBarClick = (e) => {
     setSelected(e.target.id);
   };
 
+  // 770 is medium screen size
+  React.useEffect(() => {
+    const handleResize = () => setScreenWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <nav className="bg-section-base p-4 flex items-center justify-evenly">
+    <nav className="static bg-section-base p-4 flex items-center justify-between md:justify-evenly">
       <a className="text-3xl michroma" href="/">
         Datarai.
       </a>
-      <div className="flex justify-center gap-4 grow">
-        {selected === "home" ? (
-          <button
-            id="home"
-            className="cursor-pointer text-primary bg-primary/10 p-2 rounded-lg"
-            onClick={navBarClick}
-          >
-            <b>Home</b>
-          </button>
-        ) : (
-          <button
-            id="home"
-            className="cursor-pointer p-2"
-            onClick={navBarClick}
-          >
-            Home
-          </button>
-        )}
-        {selected === "dash" ? (
-          <button
-            id="dash"
-            className="cursor-pointer text-primary bg-primary/10 p-2 rounded-lg"
-            onClick={navBarClick}
-          >
-            <b>Dashboard</b>
-          </button>
-        ) : (
-          <button
-            id="dash"
-            className="cursor-pointer p-2"
-            onClick={navBarClick}
-          >
-            Dashboard
-          </button>
-        )}
-        {selected === "about" ? (
-          <button
-            id="about"
-            className="cursor-pointer text-primary bg-primary/10 p-2 rounded-lg"
-            onClick={navBarClick}
-          >
-            <b>About</b>
-          </button>
-        ) : (
-          <button
-            id="about"
-            className="cursor-pointer p-2"
-            onClick={navBarClick}
-          >
-            About
-          </button>
-        )}
-      </div>
+      {screenWidth > 770 ? (
+        <>
+          <div className="flex justify-center gap-4 grow">
+            {selected === "home" ? (
+              <button
+                id="home"
+                className="cursor-pointer text-primary bg-primary/10 p-2 rounded-lg"
+                onClick={navBarClick}
+              >
+                <b>Home</b>
+              </button>
+            ) : (
+              <button
+                id="home"
+                className="cursor-pointer p-2"
+                onClick={navBarClick}
+              >
+                Home
+              </button>
+            )}
+            {selected === "dash" ? (
+              <button
+                id="dash"
+                className="cursor-pointer text-primary bg-primary/10 p-2 rounded-lg"
+                onClick={navBarClick}
+              >
+                <b>Dashboard</b>
+              </button>
+            ) : (
+              <button
+                id="dash"
+                className="cursor-pointer p-2"
+                onClick={navBarClick}
+              >
+                Dashboard
+              </button>
+            )}
+            {selected === "about" ? (
+              <button
+                id="about"
+                className="cursor-pointer text-primary bg-primary/10 p-2 rounded-lg"
+                onClick={navBarClick}
+              >
+                <b>About</b>
+              </button>
+            ) : (
+              <button
+                id="about"
+                className="cursor-pointer p-2"
+                onClick={navBarClick}
+              >
+                About
+              </button>
+            )}
+          </div>
 
-      <button className="flex gap-1 bg-primary/30 text-primary font-bold py-1 px-2 text-center rounded-md border-2 border-black/20">
-      <FaCrown className="self-center" /> Upgrade to Pro
-      </button>
+          <button className="flex gap-1 bg-primary/30 text-primary py-1 px-2 text-center rounded-md border-2 border-black/20">
+            <FaCrown className="self-center" /> Upgrade to Pro
+          </button>
+        </>
+      ) : (
+        <>
+          <button className="flex text-4xl">
+            {showMenu ? (
+              <IoCloseOutline
+                className="self-center"
+                onClick={() => setShowMenu(false)}
+              />
+            ) : (
+              <IoMenu
+                className="self-center"
+                onClick={() => setShowMenu(true)}
+              />
+            )}
+          </button>
+          {showMenu && (
+            <div className="absolute p-2 right-2 top-12 mt-2 bg-background rounded-lg shadow-lg">
+              {selected === "home" ? (
+                <button
+                  id="home"
+                  className="cursor-pointer text-primary bg-primary/10 p-2 rounded-lg"
+                  onClick={navBarClick}
+                >
+                  <b>Home</b>
+                </button>
+              ) : (
+                <button
+                  id="home"
+                  className="cursor-pointer p-2"
+                  onClick={navBarClick}
+                >
+                  Home
+                </button>
+              )}
+              {selected === "dash" ? (
+                <button
+                  id="dash"
+                  className="cursor-pointer text-primary bg-primary/10 p-2 rounded-lg"
+                  onClick={navBarClick}
+                >
+                  <b>Dashboard</b>
+                </button>
+              ) : (
+                <button
+                  id="dash"
+                  className="cursor-pointer p-2"
+                  onClick={navBarClick}
+                >
+                  Dashboard
+                </button>
+              )}
+              {selected === "about" ? (
+                <button
+                  id="about"
+                  className="cursor-pointer text-primary bg-primary/10 p-2 rounded-lg"
+                  onClick={navBarClick}
+                >
+                  <b>About</b>
+                </button>
+              ) : (
+                <button
+                  id="about"
+                  className="cursor-pointer p-2"
+                  onClick={navBarClick}
+                >
+                  About
+                </button>
+              )}
+              <div className="flex justify-center mt-2">
+                <button className="flex gap-1 bg-primary/30 text-primary py-1 px-2 text-center rounded-md border-2 border-black/20">
+                  <FaCrown className="self-center" /> Upgrade to Pro
+                </button>
+              </div>
+            </div>
+          )}
+        </>
+      )}
     </nav>
   );
 };
